5f112502ba9214c8a738da42a5d59ccc2e6958e2
Artem Krivoshein <artem.krivoshein@tele2.kz>
2024-08-31 22:10:21 +0500

    Добавил файл constansts вынес туда ссылку и добавил в conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,14 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from constants import BASE_URL
+
 
 
 @pytest.fixture
 def open_browser():
     driver = webdriver.Chrome()
-    driver.get("https://stellarburgers.nomoreparties.site/")
+    driver.get(BASE_URL)
     WebDriverWait(driver, 10)
     yield driver
     driver.quit()
